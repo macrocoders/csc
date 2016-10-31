34a4f396d8fdf5b02889d06d7e3c96a0e5df9055
Ivan Fefilov <ivfefilov@gmail.com>
2016-10-31 16:40:10 +0400

    stylize sign in and forgot password actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,17 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
   
+  layout :layout_by_resource
+  
   protected
+  
+  def layout_by_resource
+    if devise_controller?
+      "devise"
+    else
+      "application"
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :middle_name])
