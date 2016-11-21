4cb5f86e17a99b45b13674e6017bb9f45fbf7233
Ivan Fefilov <ivfefilov@gmail.com>
2016-11-21 16:46:52 +0400

    update header and sidebar, add ru devise translation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable #:registerable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :registerable
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
+  def full_name
+    [first_name, last_name].join(' ')
+  end  
 end
