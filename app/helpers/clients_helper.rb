d6e99602129a352402133ba5923ad5c37e87441c
Ivan Fefilov <ivfefilov@gmail.com>
2017-01-20 16:14:02 +0400

    add navbar and edit link to clients index page

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,5 +1,9 @@
 module ClientsHelper
   def legal_statuses
     [['Физ. лицо', Client::NATURAL_PERSON_STATUS], ['Юр. лицо', Client::LEGAL_ENTITY_STATUS]]
-  end  
+  end
+  
+  def client_action_links(client_id)
+    [content_tag(:li, link_to('Редактировать', edit_references_client_path(client_id)))].join.html_safe
+  end    
 end
